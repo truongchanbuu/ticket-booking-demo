1eca12ae57a977d7be8a453c52b7e707eecb7788
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-04-07 08:40:49 +0700

    base(project): init commit

---
+++ user-service/src/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`);
+});
