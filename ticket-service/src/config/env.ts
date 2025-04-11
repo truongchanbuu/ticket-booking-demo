10d25c7b2f97f422f2b238fa91e033eb80e31c71
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-04-11 16:41:14 +0700

    fix(ticket-service): fix kafka broker server and port

--- ticket-service/src/config/env.ts
+++ ticket-service/src/config/env.ts
@@ -1,13 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const KAFKA_BROKER_DEFAULT = "192.168.174.120:9202";
+const KAFKA_PORT_DEFAULT = 9020;
+const KAFKA_PORT = process.env.KAFKA_BROKER_PORT || KAFKA_PORT_DEFAULT;
+const KAFKA_BROKER_DEFAULT = `192.168.174.120:${KAFKA_PORT}`;
+
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV || "development",
+  KAFKA_PORT,
   PORT: process.env.PORT ? +process.env.PORT : 3000,
   DATABASE_URL: process.env.DATABASE_URL!,
   JWT_SECRET: process.env.JWT_SECRET!,
-  KAFKA_BROKER_1: process.env.KAFKA_BROKER_1 || KAFKA_BROKER_DEFAULT,
-  KAFKA_BROKER_2: process.env.KAFKA_BROKER_2 || KAFKA_BROKER_DEFAULT,
-  KAFKA_BROKER_3: process.env.KAFKA_BROKER_3 || KAFKA_BROKER_DEFAULT,
+  KAFKA_BROKER_1: process.env.KAFKA_BROKER_1
+    ? `${process.env.KAFKA_BROKER_1}:${KAFKA_PORT}`
+    : KAFKA_BROKER_DEFAULT,
+  KAFKA_BROKER_2: process.env.KAFKA_BROKER_2
+    ? `${process.env.KAFKA_BROKER_2}:${KAFKA_PORT}`
+    : KAFKA_BROKER_DEFAULT,
+  KAFKA_BROKER_3: process.env.KAFKA_BROKER_3
+    ? `${process.env.KAFKA_BROKER_3}:${KAFKA_PORT}`
+    : KAFKA_BROKER_DEFAULT,
 };
